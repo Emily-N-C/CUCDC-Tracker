7256777faf2b3c5fbd2daa4971f879ff7fdc9881
Dalton584 <74195094+Dalton584@users.noreply.github.com>
2023-02-17 14:45:16 -0500

    Add files via upload

--- LoginInfo_mysql_create.sql
+++ LoginInfo_mysql_create.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `User Information` (
 	`Email` TEXT NOT NULL,
-	`Username` TEXT NOT NULL,
-	`Password` TEXT NOT NULL,
+	`Username` TEXT(15) NOT NULL,
+	`Password` TEXT(11) NOT NULL,
 	PRIMARY KEY (`Username`)
 );
 
